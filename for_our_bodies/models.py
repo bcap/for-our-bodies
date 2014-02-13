3df3bc322015e8ecd94011e2b56ad5412486dbf3
Bruno Polaco <polaco@gmail.com>
2014-02-13 00:31:56 -0200

    defining basic models

--- for_our_bodies/models.py
+++ for_our_bodies/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
-class User(models.Model):
+
+class str_trait(object):
+    def __unicode__(self):
+        return ', '.join(
+            (': '.join((key, str(value))) for key, value in self.__dict__.iteritems() if not key.startswith('_'))
+        )
+
+
+class User(models.Model, str_trait):
     name = models.CharField(max_length=60)
+    sleep_hours = models.IntegerField(default=8)
 
 
-class Weight(models.Model):
+class Entry(models.Model, str_trait):
     user = models.ForeignKey(User)
-    weight = models.IntegerField(default=0)
-    when = models.DateField(auto_now=True)
+    day = models.DateField()
+    wakeup = models.DateTimeField()
+    sleep = models.DateTimeField()
+    weight = models.FloatField()
+    food = models.TextField()
